1a7cfb4b04aad20533f32608852f183abbf66627
Audrea Cook <audreacwork@gmail.com>
2019-07-30 00:16:38 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song
+  attr_accessor :name
+  attr_reader :artist
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
+  end
+  
+end
